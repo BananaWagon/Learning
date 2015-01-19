5667ad5f2d19368c9a69de68cf11b51fbda0d264
Raymond Huff <rehuff83@gmail.com>
2015-01-19 14:43:07 -0600

    Building MiniMax
    Started working with MiniMax for TTT AI.

--- Board-Games/Board-Games/TicTacToe.h
+++ Board-Games/Board-Games/TicTacToe.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "GameBase.h"
 #include "Board.h"
 
@@ -15,3 +15,30 @@ public:
 private:
 	int _gameState;
 };
+
+///////////////////////////////////////////////////////////////
+//                                                           //
+//                    Suedo for MiniMax                      //
+//                                                           //
+///////////////////////////////////////////////////////////////
+//                                                           //
+//	function minimax(node, depth, maximizingPlayer)          //
+//		if depth = 0 or node is a terminal node              //
+//			return the heuristic value of node               //
+//		if maximizingPlayer                                  //
+//			bestValue : = -∞                                 //
+//			for each child of node                           //
+//				val : = minimax(child, depth - 1, FALSE)     //
+//			  bestValue : = max(bestValue, val)              //
+//			return bestValue                                 //
+//		else                                                 //
+//			bestValue : = +∞                                 //
+//			for each child of node                           //
+//				val : = minimax(child, depth - 1, TRUE)      //
+//				bestValue : = min(bestValue, val)            //
+//			return bestValue                                 //
+//                                                           //
+//  (*Initial call for maximizing player *)                  //
+//  minimax(origin, depth, TRUE)                             //
+//                                                           //
+///////////////////////////////////////////////////////////////
